36fc5e627b98ec1c1a1500e710e33892cf94a06b
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-23 14:13:32 +0100

    Add serchCountries funcion

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 "use strict";
 (function () {
 
-    
+    var url = 'https://restcountries.eu/rest/v2/name/';
+    var countriesList = document.getElementById('countries');
+
+    document.getElementById('search').addEventListener('click', searchCountries);
+
+    function searchCountries() {
+        var countryName = document.getElementById('country-name').value;
+        if (!countryName.length) countryName = 'Poland';
+        fetch(url + countryName)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then(showCountriesList);
+    }
+
+    function showCountriesList(resp) {
+        countriesList.innerHTML = '';
+        console.log(resp);
+      }
+
 })();
